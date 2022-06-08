b9c2b51bd2b07e26856ac3ad565237e6dfe1746d
khemraj22 <khemraj.cadd@gmail.com>
2022-06-08 17:14:47 +0530

    nodejs  api with test cases.

---
+++ server.js
@@ -0,0 +1,58 @@
+let express = require('express');
+let app = express();
+let mongoose = require('mongoose');
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
+let port = 8080;
+let book = require('./routes/book');
+let config = require('config'); //we load the db location from the JSON files
+
+//db options
+// let options = {
+//     server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+//     replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
+// };
+const options = {
+    useMongoClient: true,
+    autoIndex: false, // Don't build indexes
+    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0, keepAlive: 1, connectTimeoutMS: 30000
+};
+
+mongoose.connect(config.DBHost, options);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+//don't show the log when it is test
+if (config.util.getEnv('NODE_ENV') !== 'test') {
+    //use morgan to log at command line
+    app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+}
+
+//parse application/json and look for raw text                                        
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json' }));
+
+app.get("/", (req, res) => res.json({ message: "Welcome to our Bookstore!" }));
+
+app.route("/book")
+    .get(book.getBooks)
+    .post(book.postBook);
+app.route("/book/:id")
+    .get(book.getBook)
+    .delete(book.deleteBook)
+    .put(book.updateBook);
+
+// don't listen in tests
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}!`);
+    });
+}
+
+module.exports = app;
